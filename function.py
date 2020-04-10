468e81aad3a8e756667daf6db69ae5f679e8efc3
soonest94 <soonest94@gmail.com>
2020-04-10 19:24:47 +0900

    Basic funtion_2 Plus "return"

--- function.py
+++ function.py
@@ -18,3 +18,18 @@ def chat(name1, name2, cost):
     print("%s: Cuesta %d euros." % (name2, cost))
 
 chat("Pedro", "Martin", 5)
+
+
+def dsum(a, b):
+    result = a + b
+    return result
+
+d = dsum(2, 4)
+print(d)
+
+def dsum2(a, b):
+    result = a + b
+    print(result)
+
+d = dsum2(5, 4)
+print(d)
