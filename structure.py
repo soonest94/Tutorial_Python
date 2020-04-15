a7fa68396369962390c1bbde1265de09229ab127
soonest94 <soonest94@gmail.com>
2020-04-15 19:29:27 +0900

    Basic data structure The basic usage of the data structure in Python.

---
+++ structure.py
@@ -0,0 +1,18 @@
+# list
+
+x = [1, 2, 3, 4]
+y = ["hello", "world"]
+z = ["hello", 1, 2, 3]
+
+print(x)
+print(y)
+print(z)
+
+print(x + y)    # possible to add lists.
+
+print(x[0])     # x = [1, 2, 3, 4] means
+                # x[0] = 1, x[1] = 2, x[2] = 3, x[3] = 4
+
+x[3] = 10
+print(x)        # possible to change an elemant in list.
+
