e18d1fa454270ca8849a12108939df4bb1302039
soonest94 <soonest94@gmail.com>
2020-04-22 23:11:28 +0900

    Basic data structure_2 Plus some advanced tools for the list function.

--- structure.py
+++ structure.py
@@ -1,4 +1,4 @@
-# list
+#################### list ####################
 
 x = [1, 2, 3, 4]
 y = ["hello", "world"]
@@ -8,11 +8,45 @@ print(x)
 print(y)
 print(z)
 
-print(x + y)    # possible to add lists.
+print(x + y)            # possible to add lists.
 
-print(x[0])     # x = [1, 2, 3, 4] means
-                # x[0] = 1, x[1] = 2, x[2] = 3, x[3] = 4
+print(x[0])             # x = [1, 2, 3, 4] means
+                        # x[0] = 1, x[1] = 2, x[2] = 3, x[3] = 4
 
 x[3] = 10
-print(x)        # possible to change an elemant in list.
+print(x)                # possible to change an element in list.
 
+
+####### advanced tools for list function #######
+
+x = [1, 2, 3, 4]
+
+num_elements = len(x)
+print(num_elements)     # show the length of the list
+
+
+x = [4, 2, 3, 1]
+
+y = sorted(x)           # sorting the list in order; y = [1, 2, 3, 4]
+print(y)
+
+z = sum(x)              # add all elements together
+print(z)
+
+# loop with list
+x = [4, 2, 3, 1]
+y = ["hello", "there"]
+
+for n in x:
+    print(n)
+for c in y:
+    print(c)
+
+print(x.index(3))       # find where a element is in list
+                        # CAUTION: The position of elements in list starts with 0.
+print(y.index("hello"))
+
+print("hello" in y)     # judge the element is in list; TRUE or FALSE
+
+if "hello" in y:
+    print("hello exists in the list.")
